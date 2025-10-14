01e77d6821d58a87696301767c23d1cfc4beb7d4
charan <charanreddynr@gmail.com>
2025-10-14 21:08:45 +0530

    Initial setup with .gitignore and DB config

---
+++ src/test/java/com/charan/claimsprocessingsystem/ClaimsProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.claimsprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
